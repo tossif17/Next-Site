dfb952408e35585901c2ce47e9267d655eb5b038
tossif17 <tosifbhurani05@gmail.com>
2024-02-27 19:59:25 +0530

    first section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,33 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
+  "files.associations": {
+    "*.css": "tailwindcss",
+  },
+
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    animation: {
+      spotlight: "spotlight 2s ease .75s 1 forwards",
+    },
+    keyframes: {
+      spotlight: {
+        "0%": {
+          opacity: "0",
+          transform: "translate(-72%, -62%) scale(0.5)",
+        },
+        "100%": {
+          opacity: "1",
+          transform: "translate(-50%,-40%) scale(1)",
+        },
       },
     },
   },
